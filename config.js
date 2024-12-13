bddadd286f23dac70c1a908edf4c2df1db2737ed
fkfl <fred.li@talk.tools>
2024-12-13 15:24:40 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,2 +1,2 @@
-const API_URL = "https://eff8-45-14-71-22.ngrok-free.app/api/content";
+const API_URL = "https://da86-45-14-71-22.ngrok-free.app/api/content";
 console.log("Config.js is loaded. API_URL:", API_URL);
