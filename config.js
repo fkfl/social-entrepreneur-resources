1df0df73e8702dfd481a658d370b4d4ded810a02
fkfl <fred.li@talk.tools>
2024-12-13 16:25:23 +0800

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
-console.log("Config.js file has started loading.");
 const API_URL = "https://da86-45-14-71-22.ngrok-free.app/api/content";
+
+console.log("Config.js file has started loading.");
 console.log("Config.js is loaded. API_URL:", API_URL);
