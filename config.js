3ee1065c8db7aa60f28cab8b6b1351ac4024fa23
fkfl <fred.li@talk.tools>
2024-12-13 21:51:47 +0800

    Update app.py config.js index.html ngrok url

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://4349-45-14-71-22.ngrok-free.app/api/content";
+const API_URL = "https://f9c9-14-0-157-247.ngrok-free.app/api/content";
 
 console.log("Config.js file has started loading.");
 console.log("Config.js is loaded. API_URL:", API_URL);
